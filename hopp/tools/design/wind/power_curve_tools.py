ab293b1ad1e6f952c4fd99f1f7ba3c8677dabf82
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-01-21 12:42:23 -0700

    added cp and power calculation functions

---
+++ hopp/tools/design/wind/power_curve_tools.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def calculate_cp_from_power(wind_speeds_ms,power_curve_kw,rotor_diameter,air_density = 1.225):
+    rotor_area = np.pi*((rotor_diameter/2)**2)
+    if isinstance(wind_speeds_ms,list):
+        wind_speeds_ms = np.array(wind_speeds_ms)
+    if isinstance(power_curve_kw,list):
+        power_curve_kw = np.array(power_curve_kw)
+    # power available in the wind (kW)
+    p_wind = 0.5*air_density*rotor_area*(wind_speeds_ms**3)/1e3
+    cp = list(power_curve_kw/p_wind)
+    return cp
+
+def calculate_power_from_cp(wind_speeds_ms,cp_curve,rotor_diameter,air_density = 1.225):
+    rotor_area = np.pi*((rotor_diameter/2)**2)
+    if isinstance(wind_speeds_ms,list):
+        wind_speeds_ms = np.array(wind_speeds_ms)
+    if isinstance(cp_curve,list):
+        cp_curve = np.array(cp_curve)
+    # power available in the wind (kW)
+    p_wind = 0.5*air_density*rotor_area*(wind_speeds_ms**3)/1e3
+    power_kW = list(cp_curve*p_wind)
+    return power_kW
